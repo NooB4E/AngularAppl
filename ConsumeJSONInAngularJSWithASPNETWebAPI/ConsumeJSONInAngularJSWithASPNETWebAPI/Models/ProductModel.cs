943ec620e3b1abde179b333a25adcf49284cfe63
Anatoly Sannikov <Anatoly.Sannikov.GDC@ts.fujitsu.com>
2017-09-28 10:27:44 +0300

    first commit

---
+++ ConsumeJSONInAngularJSWithASPNETWebAPI/ConsumeJSONInAngularJSWithASPNETWebAPI/Models/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security.Provider;
+
+
+namespace ConsumeJSONInAngularJSWithASPNETWebAPI.Models
+{
+    public class ProductModel
+    {
+        public List<Product> findAll()
+        {
+         List<Product> result = new List<Product>();
+            result.Add(new Product {Id = "p01", Name = "Product 1", Price = 11, Quantity = 2});
+            result.Add(new Product { Id = "p02", Name = "Product 2", Price = 23, Quantity = 3});
+        return result;
+         }
+
+        public Product find(string id)
+        {
+            return findAll().Single(p => p.Id.Equals(id));
+        }
+    }
+}
